8a5906afd91acc0de63bd1c395c7f6d7e204e80d
Duduling <duduling20@gmail.com>
2020-12-17 08:10:30 +0900

    #2.3 TypeORM Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
 import { join } from 'path'
 
 import { RestaurantsModule } from './restaurants/restaurants.module'
 
 @Module({
 	imports: [
+		TypeOrmModule.forRoot({
+		}),
 		GraphQLModule.forRoot({
 			// autoSchemaFile: true,
 			autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
